58f11db43c965cf8b64c1fcc301699efa00e4001
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2024-12-02 19:47:35 -0300

    Bugfix DbContext
    Bugfixes:
    - Remade Dbcontext

--- WebApi/WebApiDotNet/Utils/DbContext.cs
+++ WebApi/WebApiDotNet/Utils/DbContext.cs
@@ -15,6 +15,20 @@ namespace WebApiDotNet.Utils
         #region Handlers
         protected override void OnModelCreating(ModelBuilder _modelBuilder)
         {
+            _modelBuilder.Entity<Course>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Title).IsRequired().HasMaxLength(200);
+                entity.Property(c => c.Description).HasMaxLength(500);
+                entity.Property(c => c.ImageBase64).IsRequired();
+                entity.HasMany(c => c.Videos).WithOne(v => v.Course).HasForeignKey(v => v.FKCourseId).OnDelete(DeleteBehavior.Cascade);
+            });
+            _modelBuilder.Entity<CourseVideo>(entity =>
+            {
+                entity.HasKey(cv => cv.Id);
+                entity.Property(cv => cv.CourseVideoUrl).IsRequired().HasMaxLength(500);
+                entity.HasOne(cv => cv.Course).WithMany(c => c.Videos).HasForeignKey(cv => cv.FKCourseId).OnDelete(DeleteBehavior.Cascade);
+            });
             _modelBuilder.Entity<User>(entity =>
             {
                 entity.ToTable("Users");
