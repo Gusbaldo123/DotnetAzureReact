717b17c920437821fc795eead0b10b742f38a1d2
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2025-04-04 10:12:42 -0300

    Recover password text
    Added:
     - Recover text

--- WebSite/website/src/pages/RecoverPasswordPage/RecoverPasswordPage.js
+++ WebSite/website/src/pages/RecoverPasswordPage/RecoverPasswordPage.js
@@ -1,5 +1,5 @@
 //#region imports
-import React,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import UserManager from "../../utils/UserManager";
@@ -13,31 +13,44 @@ import Banner from "../../components/shared/Banner"
 export default RecoverPasswordPage;
 //#endregion
 
-//#region JSX
-function RecoverPasswordPage()
+//#region Handlers
+function Recover(e, setResponse) {
+    e.preventDefault();
+    const email = e.target.elements.emailRecover.value.toLowerCase();
+    SendMail(email)
+    setResponse(`Mail sent to '${email}'(May be on spam!)`);
+}
+function SendMail(email)
 {
+    
+}
+//#endregion
+
+//#region JSX
+function RecoverPasswordPage() {
     const navigate = useNavigate();
     const user = UserManager.getLocalUser();
+    const [response, setResponse] = useState(null);
     useEffect(() => {
         document.title = "Skillhub - Recover Password";
 
-        if(user) navigate("/Home");
-      }, [navigate]);
-    
-    return(<>
-    <Header/>
-    <Banner/>
-    <section className="RecoverPasswordContent">
-        <h2>Recover Password</h2>
-        <form action="" method="post">
-            <label htmlFor="emailRecover">Email: </label>
-            <input type="email" name="emailRecover" id="emailRecover" />
-            <input type="submit" value="Confirm" />
-        </form>
-        <br />
-        <p className="responseRecover"></p>
-    </section>
-    <Footer/>
+        if (user) navigate("/Home");
+    }, [navigate]);
+
+    return (<>
+        <Header />
+        <Banner />
+        <section className="RecoverPasswordContent">
+            <h2>Recover Password</h2>
+            <form action="" method="post" onSubmit={(e)=>Recover(e,setResponse)}>
+                <label htmlFor="emailRecover">Email: </label>
+                <input type="email" name="emailRecover" id="emailRecover" />
+                <input type="submit" value="Confirm" />
+            </form>
+            <br />
+            <p className="responseRecover">{response}</p>
+        </section>
+        <Footer />
     </>);
 }
 //#endregion
